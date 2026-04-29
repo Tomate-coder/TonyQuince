a1239b1064ecbca33247c02ff496327fc11c5ebb
Tomate-coder <correoelectronicoparacarlos@gmail.com>
2026-04-28 20:45:40 -0600

    Se creó el DELETE

---
+++ delete_artcl.php
@@ -0,0 +1,14 @@
+<?php
+include("connection.php");
+
+$con = connection();
+
+$id = $_GET["id"];
+
+$sql = "DELETE FROM articulos WHERE  id_articulo = $id";
+
+$query = mysqli_query($con, $sql);
+
+if($query) {  
+    Header("Location: index.php");
+};
